840d82d711a113c3b65f0589909c41621a2d09eb
401Unauthorized <hi@4o1.to>
2023-03-30 02:30:53 +0800

    chore: remove lint of vue/multi-word-component-names

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-useless-constructor': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off'
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'vue/multi-word-component-names': 'off'
   }
 }
